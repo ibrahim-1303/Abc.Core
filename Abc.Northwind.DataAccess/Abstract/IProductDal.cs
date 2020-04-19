7b83cde7bfc80bb0c5eeef261cb0fcd85bc1d55a
ibrahim-1303 <ibrahim.ayfer91@gmail.com>
2020-04-19 19:14:42 +0300

    Add project files.

---
+++ Abc.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,10 @@
+﻿using Abc.Core.DataAccess;
+using Abc.Northwind.Entities.Concrete;
+
+namespace Abc.Abc.Northwind.DataAccess.Abstract
+{
+    public interface IProductDal: IEntityRepository<Product>
+    {
+        //Custom Operations
+    }
+}
